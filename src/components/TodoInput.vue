d0519bdb254b1b21f4d461a9d9f37133b2ea3aa7
윤성호 <yunseongho@yunseonghoui-MacBookPro.local>
2023-05-24 14:34:10 +0900

    first commit

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
+    <!-- <button v-on:click="addTodo">add</button> -->
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="fas fa-plus addBtn" aria-hidden="true"></i>
+    </span>
+  </div>
+</template>
+<script>
+import "@fortawesome/fontawesome-free/css/all.css";
+
+export default {
+  data: function () {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo: function () {
+      if (this.newTodoItem !== "") {
+        var obj = { completed: false, item: this.newTodoItem };
+        //(텍스트,텍스트) 가 아닌 (텍스트, 불린) 을 저장
+        localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+        this.clearInput();
+      }
+    },
+    clearInput: function () {
+      this.newTodoItem = ""; //비워주기(초기화)
+    },
+  },
+};
+</script>
+
+<style scoped>
+.inputBox {
+  background: back;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  text-align: center;
+}
+
+.inputBox input {
+  text-align: center;
+  border-style: none;
+  font-size: 0.9rem;
+  flex: 1;
+  height: 100%;
+}
+
+.addContainer {
+  background: linear-gradient(to right, #62eac6, #32cee6);
+  width: 50px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.addBtn {
+  color: white;
+}
+</style>
